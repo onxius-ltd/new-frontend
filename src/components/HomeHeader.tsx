d6884d4f6400c7adff937698e5ea1aad48904a4a
talha rafiq <talha.swiss.tech@gmail.com>
2026-02-11 01:11:54 +0500

    bugs fixed and few new improvements + google analytics integration

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -6,40 +6,40 @@ import Image from "next/image";
 export default function HomeHeader() {
 
       return (
-            <div>
+            <div className="border">
                   {/* Hero Header Start */}
-                  <div className="hero-header overflow-hidden px-5">
+                  <div className="hero-header overflow-hidden md:mt-5" >
                         {/* animated rotated image */}
                         <div className="rotate-img">
                               <Image src="/assets/img/sty-1.png" className="img-fluid w-100" alt="" fill />
                               <div className="rotate-sty-2" />
                         </div>
                         {/* header content */}
-                        <div className="row gy-5 align-items-center">
+                        <div className="row align-items-center">
                               {/* left side content */}
                               <div className="col-lg-6 wow fadeInLeft" data-wow-delay="0.1s">
                                     <h1
-                                          className="display-5 text-dark mb-4 wow fadeInUp"
+                                          className="text-2xl sm:text-3xl 2xl:text-4xl text-dark mb-3 md:mb-4 wow fadeInUp xl:max-w-2xl"
                                           data-wow-delay="0.3s"
                                           style={{ fontWeight: 500 }}
                                     >
                                           We Build Scalable Web & SaaS Solutions for Growing Businesses
                                     </h1>
-                                    <p className="fs-4 mb-4 wow fadeInUp" data-wow-delay="0.5s">
+                                    <p className="text-xl mb-3 md:mb-4 wow fadeInUp xl:max-w-2xl" data-wow-delay="0.5s">
                                           Custom web development, SaaS platforms, and IT solutions designed to increase revenue and automate operations.
                                     </p>
                                     {/* cta button */}
                                     <div className="d-flex gap-3 flex-wrap">
                                           <Link
                                                 href="/contact"
-                                                className="btn butn-custom-outline rounded-pill py-3 px-5 wow fadeInUp"
+                                                className="btn butn-custom-outline rounded-pill py-3 px-5 wow fadeInUp w-full md:w-fit"
                                                 data-wow-delay="0.7s"
                                           >
                                                 Get a Free Consultation &#x2799;
                                           </Link>
                                           <Link
                                                 href="/portfolio"
-                                                className="btn butn-custom rounded-pill py-3 px-5 wow fadeInUp"
+                                                className="btn butn-custom rounded-pill py-3 px-5 wow fadeInUp  w-full md:w-fit"
                                                 data-wow-delay="0.7s"
                                           >
                                                 View Our Work &#x2799;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,61 +1,78 @@
 "use client"; // 👈 add this if you plan to use interactivity (like mobile toggle)
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomeHeader from "./HomeHeader";
 import { usePathname } from "next/navigation";
 import Header from "./Header";
 
+type menuItem = { label: string; href: string };
+
+const menus: menuItem[] = [
+      {
+            label: "Home",
+            href: "/"
+      },
+      {
+            label: "Services",
+            href: "/services"
+      },
+      {
+            label: "Portfolio",
+            href: "/portfolio"
+      },
+      {
+            label: "About",
+            href: "/about"
+      },
+      {
+            label: "Contact Us",
+            href: "/contact"
+      }
+]
 
 export default function Navbar() {
       const pathname = usePathname();
-      const [isOpen, setIsOpen] = useState(false);
-      const menus = [
-            {
-                  label: "Home",
-                  href: "/"
-            },
-            {
-                  label: "Services",
-                  href: "/services"
-            },
-            {
-                  label: "Portfolio",
-                  href: "/portfolio"
-            },
-            {
-                  label: "About",
-                  href: "/about"
-            },
-            {
-                  label: "Contact Us",
-                  href: "/contact"
-            }
-      ]
+      const [isOpen, setIsOpen] = useState<boolean>(false);
+      // const [width, setWidth] = useState<number>(0)
+
+      // useEffect(() => {
+      //       if (typeof window == undefined) {
+      //             return;
+      //       }
+      //       setWidth(window.innerWidth)
+      // }, []);
+
+      // console.log("isopend = ", (!isOpen && width < 992 ? "none !important" : "block !important"))
 
       return (
             <div>
                   {/* Navbar & Hero Start */}
-                  <div className="container-fluid header position-relative overflow-hidden p-0">
+                  <div className="container-fluid header overflow-hidden p-0 sticky-top">
                         {/* navbar */}
-                        <nav className="navbar navbar-expand-lg fixed-top navbar-light px-4 px-lg-5 py-3 py-lg-0 d-flex justify-content-between align-items-cetner gap-2 w-full">
+                        <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-2 py-lg-0 d-flex justify-content-between align-items-cetner gap-2 w-full">
                               {/* nav logo */}
                               <Link href="/" className="navbar-brand p-0">
-                                    <Image src="/assets/logo/new-onxius-logo-without-bg.png" className="img-fluid w-100 h-100" alt="ONXIUS Logo" width={240} height={85} />
+                                    <Image src="/assets/logo/new-onxius-logo-without-bg.png" className="img-fluid w-auto h-40 sm:h-100" alt="ONXIUS Logo" width={240} height={65} />
                               </Link>
                               {/* nav toggler */}
                               <button
-                                    className="navbar-toggler"
+                                    // className="navbar-toggler"
                                     type="button"
-                                    data-bs-toggle="collapse"
-                                    data-bs-target="#navbarCollapse"
+                                    // data-bs-toggle="collapse"
+                                    // data-bs-target="#navbarCollapse"
                                     onClick={() => setIsOpen(!isOpen)}
+                                    className="md:hidden text-[var(--dark-blue-clr)] border-[var(--dark-blue-clr)] text-2xl"
+                              // style={{ color: "var(--dark-blue-clr)] !important", borderColor: "var(--dark-blue-clr)] !important" }}
                               >
-                                    <span className="fa fa-bars" />
+                                    {/* {isOpen ? "X" : "open"} */}
+                                    {isOpen ? "X" : <span className="fa fa-bars text-2xl" />}
                               </button>
                               {/* menus */}
-                              <div className={`navbar ${isOpen ? "show" : ""}`} id="navbarCollapse">
-                                    <div className="navbar-nav ms-auto py-0">
+                              <div className={`flex flex-column md:flex-row justify-content-center md:justify-content-end align-items-center flex-1 toggleNav ${isOpen ? "show" : "hide"}`}>
+                                    {/* menus */}
+                                    <div className="navbar-nav ms-auto py-0 align-items-center">
+                                          {/* menu options */}
                                           {
                                                 menus?.length > 0 && menus?.map((item, index) =>
                                                       <Link href={item.href} className={`nav-item nav-link ${index == 0 ? "active" : ""}`} key={index}>
@@ -63,13 +80,16 @@ export default function Navbar() {
                                                       </Link>
                                                 )
                                           }
+                                          {/* let's connect button */}
+                                          {/* <div className="border talk-button"> */}
+                                          <Link
+                                                href="/contact"
+                                                className="nav-item nav-link btn butn-custom rounded-pill py-2 px-4 talk-button"
+                                          >
+                                                Let&apos;s Talk &#x2799;
+                                          </Link>
+                                          {/* </div> */}
                                     </div>
-                                    <Link
-                                          href="/contact"
-                                          className="btn butn-custom rounded-pill py-2 px-4"
-                                    >
-                                          Let&apos;s Talk &#x2799;
-                                    </Link>
                               </div>
                         </nav>
                         {/* Hero Header Start */}
